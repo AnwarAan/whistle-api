b3477349aa2f5a265436908277222d41b82e6735
mcnwr76 <anwaraan998@gmail.com>
2024-05-30 10:18:54 +0700

    first init java api

---
+++ src/main/java/whistle/whistle_api/WhistleApiApplication.java
@@ -0,0 +1,13 @@
+package whistle.whistle_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhistleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhistleApiApplication.class, args);
+	}
+
+}
