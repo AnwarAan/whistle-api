14a36f35dae3c52002e6e047c33f4d9c083606fa
mcnwr76 <anwaraan998@gmail.com>
2024-06-19 08:37:35 +0700

    Crud Post

---
+++ src/main/java/whistle/whistle_api/controller/PostController.java
@@ -0,0 +1,48 @@
+package whistle.whistle_api.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import whistle.whistle_api.dto.ResponseData;
+import whistle.whistle_api.model.Post;
+import whistle.whistle_api.service.PostService;
+
+@RestController
+@RequestMapping("/api/post")
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<ResponseData<List<Post>>> findAllPost() {
+        List<Post> posts = postService.findAllPost();
+        return ResponseEntity.ok(ResponseData.responseSucceess(posts));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseData<Post>> findPostById(@PathVariable Long id) {
+        Post post = postService.findPostById(id);
+        return ResponseEntity.ok(ResponseData.responseSucceess(post));
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseData<Object>> createPost(@RequestParam Long userId, @RequestParam String post,
+            @RequestParam String imageUrl) {
+        postService.createPost(userId, post, imageUrl);
+        return ResponseEntity.ok(ResponseData.responseSucceess());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseData<Object>> deletePost(@PathVariable Long id) {
+        return ResponseEntity.ok(ResponseData.responseSucceess());
+    }
+}
---
+++ src/main/java/whistle/whistle_api/service/PostService.java
@@ -0,0 +1,53 @@
+package whistle.whistle_api.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import whistle.whistle_api.model.Post;
+import whistle.whistle_api.model.User;
+import whistle.whistle_api.repository.PostRepository;
+import whistle.whistle_api.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    @Autowired
+    private final PostRepository postRepository;
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public List<Post> findAllPost() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(this::mapToPost).toList();
+    }
+
+    public Post findPostById(Long id) {
+        Post post = postRepository.findById(id).orElseThrow();
+        return post;
+    }
+
+    public void createPost(Long id, String post, String imageUrl) {
+        User user = userRepository.findById(id).orElseThrow();
+        Post posts = Post.builder().post(post).imageUrl(imageUrl).user(user).createdAt(new Date()).updatedAt(new Date())
+                .build();
+
+        user.getPosts().add(posts);
+        postRepository.save(posts);
+    }
+
+    public void deletePost(Long id) {
+        postRepository.deleteById(id);
+    }
+
+    private Post mapToPost(Post post) {
+        return Post.builder().id(post.getId()).post(post.getPost()).imageUrl(post.getImageUrl())
+                .createdAt(post.getCreatedAt()).updatedAt(post.getUpdatedAt())
+                .build();
+    }
+
+}
