5673c98e9e501e147e37feca8e337586ad0d9135
mcnwr76 <anwaraan998@gmail.com>
2024-07-03 20:47:45 +0700

    updated

---
+++ src/main/java/whistle/whistle_api/dto/ResponseError.java
@@ -0,0 +1,39 @@
+package whistle.whistle_api.dto;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseError {
+  private final boolean status = false;
+  private int statusCode;
+  private String message;
+
+  public static ResponseError notFound(String msg) {
+    return ResponseError.builder().statusCode(HttpStatus.NOT_FOUND.value()).message(msg).build();
+  }
+
+  public static ResponseError internalServer() {
+    return ResponseError.builder().statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+        .message("Internal Server Error").build();
+  }
+
+  public static ResponseError forbidden(String msg) {
+    return ResponseError.builder().statusCode(HttpStatus.FORBIDDEN.value()).message(msg).build();
+  }
+
+  public static ResponseError unauthorized(String msg) {
+    return ResponseError.builder().statusCode(HttpStatus.UNAUTHORIZED.value()).message(msg).build();
+  }
+
+  public static ResponseError jwtExpired(String msg) {
+    return ResponseError.builder().statusCode(HttpStatus.FORBIDDEN.value()).message(msg).build();
+  }
+}
---
+++ src/main/java/whistle/whistle_api/helper/ExtractUser.java
@@ -0,0 +1,31 @@
+package whistle.whistle_api.helper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import whistle.whistle_api.model.User;
+import whistle.whistle_api.repository.UserRepository;
+import whistle.whistle_api.security.JwtService;
+
+@Component
+@RequiredArgsConstructor
+public class ExtractUser {
+
+  private final JwtService jwtService;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  public User extractUser(@NonNull HttpServletRequest request) {
+    String authHeader = request.getHeader("Authorization");
+    String jwtToken;
+    String userEmail;
+    jwtToken = authHeader.substring(7);
+    userEmail = jwtService.extractUsername(jwtToken);
+    User user = userRepository.findByEmail(userEmail).orElseThrow();
+    return user;
+  }
+}
