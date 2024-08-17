bf85326f6b01c539167a9a0519d36b6f9b969c73
mcnwr76 <anwaraan998@gmail.com>
2024-08-18 06:15:39 +0700

    create test feture

---
+++ src/main/java/whistle/whistle_api/controller/TestController.java
@@ -0,0 +1,26 @@
+package whistle.whistle_api.controller;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import whistle.whistle_api.dto.ResponseData;
+import whistle.whistle_api.service.TestService;
+
+@RestController
+public class TestController {
+  @Autowired
+  TestService testService;
+
+  @PostMapping("/test")
+  public ResponseEntity<ResponseData<?>> addUser(@RequestParam String name,
+      @RequestParam String email, @RequestParam String password, @RequestParam LocalDate dob,
+      @RequestParam String role) {
+    testService.addUser(name, email, password, dob, role);
+    return ResponseEntity.ok(ResponseData.responseSucceess());
+  }
+}
---
+++ src/main/java/whistle/whistle_api/service/TestService.java
@@ -0,0 +1,30 @@
+package whistle.whistle_api.service;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import whistle.whistle_api.model.User;
+import whistle.whistle_api.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  private final PasswordEncoder passwordEncoder;
+
+  public void addUser(String name, String email, String password, LocalDate dob, String role) {
+    User user = User.builder().name(name).email(email)
+        .password(passwordEncoder.encode(password)).dob(dob).role(role)
+        .status(true)
+        .createAt(new Date()).updateAt(new Date()).build();
+    userRepository.save(user);
+  }
+}
